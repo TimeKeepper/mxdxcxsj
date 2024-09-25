94044047e83870e3f28bdc6a1c485151518f48ea
wenjiu <2784307979@qq.com>
2024-09-25 18:42:54 +0800

    完成任务二

--- project2/src/main.cpp
+++ project2/src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <csignal>
+#include <cmath>
 
 volatile std::sig_atomic_t terminate = 0;
 
@@ -41,10 +42,36 @@ uint64_t Solution::get_num(uint64_t a, uint64_t n){
     return ret;
 }
 
+bool is_shuixianhua(uint16_t num){
+    if (num < 100) return false;
+    else if (num > 999) return false;
+
+    uint8_t f, s, t;
+    f = num / 100;
+    s = (num % 100) / 10;
+    t = num % 10;
+
+    if (std::pow(f, 3) + std::pow(s, 3) + std::pow(t, 3) == num) return true;
+
+    return false;
+}
+
+void print_shuixianhua(void){
+    uint16_t num = 100;
+    while (num < 1000){
+        if (is_shuixianhua(num)) std::cout << num << std::endl;
+        num++;
+    }
+}
+
 int main(int argc, char** argv) {
     std::signal(SIGINT, signal_handler);
     std::cout << "Press Ctrl+C to terminate the program..." << std::endl;
 
+    std::cout << "Printing all shuixianhua numbers between 100 and 999..." << std::endl;
+    print_shuixianhua();
+
+    std::cout << "Enter a and n to get the sum of the first n numbers of a: " << std::endl;
     Solution s;
     uint64_t test_a, test_n;
     while (!terminate){
